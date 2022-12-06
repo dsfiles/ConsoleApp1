14954c4a0a8dc2cf25d9c828c07198a411b442ca
Guangyan Li <GLi@stmartin.edu>
2022-12-06 09:31:38 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+public class Example
+{
+    public static void Main()
+    {
+        // Create the link list.
+        string[] words = { "the", "fox", "jumps", "over", "the", "dog" };
+        var sentence = new LinkedList<string>(words);
+        Display(sentence, "The linked list values:");  
+    }
+    private static void Display(LinkedList<string> words, string test) {
+        Console.WriteLine(test);
+        foreach (string word in words)
+        {
+            Console.Write(word + " ");
+        }
+        Console.Write("\n\n");
+    }
+} // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=net-5.0
